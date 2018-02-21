d524042ef09219bf0a19bf04ceab54097071a685
Christian Steimel <me@C14L.com>
2018-02-21 23:17:50 +0100

    simple.sh change img size

--- simple.sh
+++ simple.sh
@@ -3,7 +3,7 @@
 IMGDIR="/home/pi/temp/raspicam"
 
 while [ true ]; do 
-  raspistill -w 1280 -h 960 -q 70 -o "$IMGDIR/`date +%s`.jpg"
+  raspistill -w 800 -h 600 -q 70 -o "$IMGDIR/`date +%s`.jpg"
   sleep 1
 done
 
