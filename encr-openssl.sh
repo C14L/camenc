5d651e1fa0634c09891cdf1df14fb09869475935
Christian Steimel <me@C14L.com>
2018-02-22 21:43:30 +0100

    Change sizes of pics

--- encr-openssl.sh
+++ encr-openssl.sh
@@ -2,12 +2,14 @@
 
 DATADIR=$2
 INTERVAL=$1
+#PICSIZE=1920x1080
+PICSIZE=960x540
 
 echo "Writing images to: $DATADIR until Ctrl+C is pressed ..."
 
 while [ true ]; do
   FILENAME=$DATADIR/webcam-`date +%s`.jpg.enc
-  fswebcam -r 1920x1080 --skip 20 --jpeg 80 - | \
+  fswebcam -r $PICSIZE --skip 2 --jpeg 80 - | \
     openssl smime -encrypt -binary -aes-256-cbc -outform DER public-key.pem | \
     tee $FILENAME >/dev/null 2>&1
   echo "File written: $FILENAME"
--- simple2.sh
+++ simple2.sh
@@ -7,7 +7,7 @@ echo "Writing images to: $DATADIR until Ctrl+C is pressed ..."
 
 while [ true ]; do
   FILENAME=$DATADIR/webcam-`date +%s`.jpg
-  fswebcam -r 960x540 --skip 2 --jpeg 70 - | \
+  fswebcam -r 1600x1200 --skip 3 --jpeg 70 - | \
     # openssl smime -encrypt -binary -aes-256-cbc -outform DER public-key.pem | \
     tee $FILENAME >/dev/null 2>&1
   echo "File written: $FILENAME"
