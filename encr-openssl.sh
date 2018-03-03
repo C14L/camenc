dfa950de1bf8ed2c581acdcf05cbf772e57a5924
Christian Steimel <me@C14L.com>
2018-03-03 21:50:35 +0100

    encr-openssl: Create device specific hash

--- encr-openssl.sh
+++ encr-openssl.sh
@@ -5,12 +5,23 @@ DATADIR=/home/pi/temp/raspicam
 INTERVAL=3
 #PICSIZE=1920x1080
 PICSIZE=960x540
+CONFFILE=~/.camenc
 
 #echo "Writing images to: $DATADIR until Ctrl+C is pressed ..."
 
+# Load or create a hash identifier for this device.
+#
+[ -f $CONFFILE ] && \
+    HASH=`cat $CONFFILE` || \
+    HASH=`cat /sys/class/net/*/address | md5sum | cut -f1 -d" "` && \
+    echo $HASH > $CONFFILE
+
+# Take pictures forever.
+#
 while [ true ]; do
 
   FILENAME=$DATADIR/webcam-`date +%s`.jpg.enc
+  #FILENAME=$HASH
   
   fswebcam -r $PICSIZE --skip 2 --jpeg 80 - | \
     openssl smime \
