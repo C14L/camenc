66b010573d5b3a54077c5d5655780566bb70e8ed
Christian Steimel <me@C14L.com>
2018-04-13 22:09:27 +0200

    encr-openssh: Add pic uploading

--- encr-openssl.sh
+++ encr-openssl.sh
@@ -2,10 +2,11 @@
 
 EXECDIR=/home/pi/dev/camenc
 DATADIR=/home/pi/temp/raspicam
-INTERVAL=3
+INTERVAL=1
 #PICSIZE=1920x1080
 PICSIZE=960x540
 CONFFILE=~/.camenc
+POSTURL=http://192.168.0.94:8000/add
 
 #echo "Writing images to: $DATADIR until Ctrl+C is pressed ..."
 
@@ -16,12 +17,13 @@ CONFFILE=~/.camenc
     HASH=$(echo "$(date) $(cat /sys/class/net/*/address)" | md5sum | cut -f1 -d' ') && \
     echo $HASH > $CONFFILE
 
+# http --form POST http://192.168.0.94:8000/add uid=123abc456def file@webcam-1523646646.jpg.enc
+
 # Take pictures forever.
 #
 while [ true ]; do
 
   FILENAME=$DATADIR/webcam-`date +%s`.jpg.enc
-  #FILENAME=$HASH
   
   fswebcam -r $PICSIZE --skip 2 --jpeg 80 - | \
     openssl smime \
@@ -30,6 +32,8 @@ while [ true ]; do
         -aes-256-cbc \
         -outform DER $EXECDIR/public-key.pem | \
       tee $FILENAME >/dev/null 2>$DATADIR/error.log
+
+  http --form POST $POSTURL uid=$HASH file@$FILENAME
 
   #echo "File written: $FILENAME"
   sleep $INTERVAL
