06db7555738ecaa16446b7fa8a5af584242dd1d6
Christian Steimel <me@C14L.com>
2019-02-17 20:14:23 +0100

    server: Update Django

--- server/camencserver/urls.py
+++ server/camencserver/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
-from django.contrib.auth.views import login, logout
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 import main.views
 
 urlpatterns = [
-    path('camenc/login/', login, name='user_login'),
-    path('camenc/logout/', logout, name='user_logout'),
+    path('camenc/login/', LoginView.as_view(), name='user_login'),
+    path('camenc/logout/', LogoutView.as_view(), name='user_logout'),
     path('camenc/admin/', admin.site.urls, name='admin'),
     path('camenc/', main.views.home),
     path('camenc/add', main.views.add),
