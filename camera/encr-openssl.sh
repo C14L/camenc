feafe7cb033135c957f564c90a1cfe9c2f975e51
Christian Steimel <me@C14L.com>
2019-03-30 14:10:51 +0100

    camera: Split pipe to create preview pics too

--- camera/encr-openssl.sh
+++ camera/encr-openssl.sh
@@ -2,10 +2,17 @@
 
 EXECDIR=/home/pi/dev/camenc/camera
 DATADIR=/home/pi/temp/raspicam
-INTERVAL=3
+ERRLOG=$DATADIR/error.log
+
+INTERVAL=2
+
 PICSIZE=1920x1080
 #PICSIZE=960x540
+
+THUMBSIZE=48x27
+
 CONFFILE=~/.camenc
+
 #POSTURL="http://192.168.0.94:7700/add"
 POSTURL="https://c14l.com/camenc/add"
 
@@ -22,26 +29,25 @@ POSTURL="https://c14l.com/camenc/add"
 #
 while [ true ]; do
 
-  FILENAME=$DATADIR/webcam-`date +%s`.jpg.enc
+    THUMBFILE=$DATADIR/webcam-`date +%s`.preview.jpg
+    FILENAME=$DATADIR/webcam-`date +%s`.jpg.enc
 
-  fswebcam -r $PICSIZE --skip 2 --jpeg 80 - | \
-    openssl smime \
-        -encrypt \
-        -binary \
-        -aes-256-cbc \
-        -outform DER "$EXECDIR/public-key-a.pem" | \
-    openssl smime \
-        -encrypt \
-        -binary \
-        -aes-256-cbc \
-        -outform DER "$EXECDIR/public-key-b.pem" | \
-    tee $FILENAME >/dev/null 2>$DATADIR/error.log
+    fswebcam -r $PICSIZE --skip 2 --jpeg 80 - | tee                                                 \
+        >(                                                                                          \
+            convert -resize $THUMBSIZE - - > $THUMBFILE 2>$ERRLOG &&                                \
+            curl -m5 -F "uid=$HASH" -F "file=@$THUMBFILE" $POSTURL                                  \
+        )                                                                                           \
+        >(                                                                                          \
+            openssl smime -encrypt -binary -aes-256-cbc -outform DER "$EXECDIR/public-key-a.pem" |  \
+            openssl smime -encrypt -binary -aes-256-cbc -outform DER "$EXECDIR/public-key-b.pem" |  \
+            tee $FILENAME >/dev/null &&                                                             \
+            curl -m5 -F "uid=$HASH" -F "file=@$FILENAME" $POSTURL                                   \
+        )                                                                                           \
+        >/dev/null 2>$ERRLOG
 
-  curl -F "uid=$HASH" -F "file=@$FILENAME" $POSTURL
-
-  sleep $INTERVAL
-  
-  rm -f "$FILENAME"
+    sleep $INTERVAL
+    rm -f "$THUMBFILE"
+    rm -f "$FILENAME"
 
 done
 
