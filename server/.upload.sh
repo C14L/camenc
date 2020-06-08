24909a9166c5ae10961f0e81288f4fad268bbb02
Christian Steimel <me@C14L.com>
2020-06-08 12:16:50 +0200

    Update upload script

--- server/.upload.sh
+++ server/.upload.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-SRC=$( cd "$( dirname "$0" )"; pwd )
+[ -z "$SERVER01" ] && echo "Error: SERVER01 envvar not set." && exit 1
 
-SVR="$SERVER01"
-DST="$SVR:/opt/camenc/"
+SRC=$( cd "$( dirname "$0" )"; pwd )
+DST="$SERVER01:/opt/camenc/"
 
 echo "${SRC} >>> ${DST}"
-rsync -rtvP --delete --exclude=*.log --exclude=db.sqlite3 ${SRC} ${DST}
+rsync -rtvP --delete --exclude=*.log --exclude=db.sqlite3 --exclude=**/__pycache__ ${SRC} ${DST}
 
-pass webdev/server01-chris | head -n1 | ssh -tt ${SVR} "sudo systemctl restart camenc.service"
-
+pass webdev/server01-chris | head -n1 | ssh -tt ${SERVER01} "sudo systemctl restart camenc.service"
