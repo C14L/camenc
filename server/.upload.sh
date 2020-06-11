66a9057d18b5df9ca80419b07e83430ee3b33da6
Christian Steimel <me@C14L.com>
2020-06-11 21:58:37 +0200

    server: Style

--- server/.upload.sh
+++ server/.upload.sh
@@ -9,3 +9,4 @@ echo "${SRC} >>> ${DST}"
 rsync -rtvP --delete --exclude=*.log --exclude=db.sqlite3 --exclude=**/__pycache__ ${SRC} ${DST}
 
 pass webdev/server01-chris | head -n1 | ssh -tt ${SERVER01} "sudo systemctl restart camenc.service"
+
