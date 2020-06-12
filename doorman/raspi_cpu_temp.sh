100fc00a536ec416f4748419c95e4264ca58369c
Christian Steimel <me@C14L.com>
2020-06-12 12:50:09 +0200

    raspi_cpu_temp.sh: Print in one line

--- doorman/raspi_cpu_temp.sh
+++ doorman/raspi_cpu_temp.sh
@@ -5,6 +5,5 @@ cpuTemp1=$(($cpuTemp0/1000))
 cpuTemp2=$(($cpuTemp0/100))
 cpuTempM=$(($cpuTemp2 % $cpuTemp1))
 
-echo CPU temp"="$cpuTemp1"."$cpuTempM"'C"
-echo GPU $(/opt/vc/bin/vcgencmd measure_temp)
+echo "$(date) CPU temp=$cpuTemp1.$cpuTempM'C GPU $(/opt/vc/bin/vcgencmd measure_temp)"
 
